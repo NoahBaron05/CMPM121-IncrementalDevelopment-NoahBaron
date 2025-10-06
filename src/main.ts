ce9bb911dd4593700e659cbb34c6e53e593d7c2b
NoahBaron05 <156266104+NoahBaron05@users.noreply.github.com>
2025-10-06 22:09:36 +0000

    added counter increment (Step 2 Complete)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+let counter = 0;
+
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-  <button style="width: 50px; height: 50px; font-size: 25px;">⛄
+  <p style="font-size: 20px;">Snowmen ⛄: <span id="counter">0</span></p>
+  <button style="width: 50px; height: 50px; font-size: 25px;" id="increment">⛄
 `;
+
+const button = document.getElementById("increment")!;
+const counterElement = document.getElementById("counter")!;
+
+let displayCount: string = "0";
+
+button.addEventListener("click", () => {
+  counter++;
+  displayCount = `${counter}`;
+  counterElement.textContent = displayCount;
+});
