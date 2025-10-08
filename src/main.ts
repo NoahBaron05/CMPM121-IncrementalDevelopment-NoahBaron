121d5872d9879d4b8350e65f24a2c466989994db
NoahBaron05 <156266104+NoahBaron05@users.noreply.github.com>
2025-10-08 22:42:46 +0000

    Added growth rate and number of upgrades purchased

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,14 @@ document.body.innerHTML = `
   <div style="font-size: 20px;">  
     Snowmen ⛄: <span id="counter">0</span>
   </div>
+  <div style="font-size: 15px;">
+    <span id="growth">0</span> Snowmen per Second
   <div style="margin-top: 10px;">
     <button style="width: 50px; height: 50px; font-size: 25px;" id="increment">⛄
   </div>
   <div style="margin-top: 10px;">
-    <button style="width: 100px; height: 25px; font-size: 12px;" id="auto" disabled>Auto Snowmen
+    <button style="width: 100px; height: 25px; font-size: 12px;" id="auto" disabled>Auto Snowmen</button>
+    <span id="autoCounter">
   </div>
   <div style="font-size: 15px;">
     Cost: 10 Snowmen
@@ -20,8 +23,12 @@ document.body.innerHTML = `
 const snowManButton = document.getElementById("increment")!;
 const autoButton = document.getElementById("auto")! as HTMLButtonElement;
 const counterElement = document.getElementById("counter")!;
+const autoCounterElement = document.getElementById("autoCounter")!;
+const snowmenPerSecondElement = document.getElementById("growth")!;
 
 let displayCount: string = "0";
+let autoDisplayCount: number = 0;
+let growthRate: number = 0;
 
 snowManButton.addEventListener("click", () => {
   incrementCounter(1);
@@ -29,8 +36,11 @@ snowManButton.addEventListener("click", () => {
 
 autoButton.addEventListener("click", () => {
   if (counter >= 10) {
-    growthRate++;
+    numAuto++;
     counter -= 10;
+
+    autoDisplayCount++;
+    autoCounterElement.textContent = `${autoDisplayCount}`;
     autoButton.disabled = true;
   }
 });
@@ -42,14 +52,27 @@ function incrementCounter(amount: number) {
 }
 
 let lastTime: number | null = null;
-let growthRate: number = 0;
+let numAuto: number = 0;
+let snowmenCountPerSecond: number = 0;
+let timeAccumulator: number = 0;
 
 function gameLoop(currentTime: number) {
-  //Finds the increment value based on the time passed
   if (lastTime != null) {
+    //Finds the increment value based on the time passed
     const deltaTime = currentTime - lastTime;
-    const incrementAmount = (deltaTime / 1000) * growthRate;
+    timeAccumulator += deltaTime;
+
+    const incrementAmount = (deltaTime / 1000) * numAuto;
     incrementCounter(incrementAmount);
+
+    //Finds the current snowmen per second
+    snowmenCountPerSecond += incrementAmount;
+    if (timeAccumulator >= 1000) {
+      growthRate = snowmenCountPerSecond;
+      snowmenPerSecondElement.textContent = `${growthRate.toFixed(1)}`;
+      snowmenCountPerSecond = 0;
+      timeAccumulator = 0;
+    }
   }
 
   if (counter >= 10) {
