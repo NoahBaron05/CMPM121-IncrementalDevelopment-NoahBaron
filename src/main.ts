51dfe6bb51886bd4343d7d185e877a5828265f38
NoahBaron05 <156266104+NoahBaron05@users.noreply.github.com>
2025-10-08 19:10:07 +0000

    Added upgrading feature (Step 5 Complete)

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,36 @@ import "./style.css";
 let counter = 0;
 
 document.body.innerHTML = `
-  <p style="font-size: 20px;">Snowmen ⛄: <span id="counter">0</span></p>
-  <button style="width: 50px; height: 50px; font-size: 25px;" id="increment">⛄
+  <div style="font-size: 20px;">  
+    Snowmen ⛄: <span id="counter">0</span>
+  </div>
+  <div style="margin-top: 10px;">
+    <button style="width: 50px; height: 50px; font-size: 25px;" id="increment">⛄
+  </div>
+  <div style="margin-top: 10px;">
+    <button style="width: 100px; height: 25px; font-size: 12px;" id="auto" disabled>Auto Snowmen
+  </div>
+  <div style="font-size: 15px;">
+    Cost: 10 Snowmen
+  </div>
 `;
 
-const button = document.getElementById("increment")!;
+const snowManButton = document.getElementById("increment")!;
+const autoButton = document.getElementById("auto")! as HTMLButtonElement;
 const counterElement = document.getElementById("counter")!;
 
 let displayCount: string = "0";
 
-button.addEventListener("click", () => {
+snowManButton.addEventListener("click", () => {
   incrementCounter(1);
+});
+
+autoButton.addEventListener("click", () => {
+  if (counter >= 10) {
+    growthRate++;
+    counter -= 10;
+    autoButton.disabled = true;
+  }
 });
 
 function incrementCounter(amount: number) {
@@ -23,13 +42,18 @@ function incrementCounter(amount: number) {
 }
 
 let lastTime: number | null = null;
+let growthRate: number = 0;
 
 function gameLoop(currentTime: number) {
   //Finds the increment value based on the time passed
   if (lastTime != null) {
     const deltaTime = currentTime - lastTime;
-    const incrementAmount = deltaTime / 1000;
+    const incrementAmount = (deltaTime / 1000) * growthRate;
     incrementCounter(incrementAmount);
+  }
+
+  if (counter >= 10) {
+    autoButton.disabled = false;
   }
 
   //End of loop
